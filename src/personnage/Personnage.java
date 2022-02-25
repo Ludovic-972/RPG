a16bd0147776b25da6e2ff50c5385bc5b896e96c
Ludovic-972 <ludovicmathurin1@gmail.com>
2022-02-25 11:12:11 +0100

    initial commit

---
+++ src/personnage/Personnage.java
@@ -0,0 +1,18 @@
+package personnage;
+
+public abstract class Personnage {
+
+    private String name;
+    private int hp;
+    private int maxHp;
+    private int attackPoints;
+    private int defensePoints;
+
+
+
+
+
+
+
+
+}
